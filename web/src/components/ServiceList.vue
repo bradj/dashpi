3a0c8ba28a6483f9a140e55ad453cbd09ed19212
Brad Janke <bradj@users.noreply.github.com>
2024-02-12 21:03:25 -0600

    enter and space navigate

--- web/src/components/ServiceList.vue
+++ web/src/components/ServiceList.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { ref, watch, onMounted, onBeforeUnmount } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { useRouter } from 'vue-router'
 import { useServiceStore, type Service } from '../stores/services'
 
+const router = useRouter()
 const svcStore = useServiceStore()
 const services = ref<Service[]>(svcStore.services)
 
@@ -58,6 +60,8 @@ const hanelKeyup = (e: KeyboardEvent) => {
     moveDown()
   } else if (e.key === 'Escape') {
     selectedIdx.value = 0
+  } else if (e.key === 'Enter' || e.key === ' ') {
+    router.push({ name: 'service', params: { id: services.value[selectedIdx.value].name } })
   }
 }
 
@@ -70,7 +74,7 @@ const hanelKeyup = (e: KeyboardEvent) => {
       class="w-full max-h-52 flex
       rounded-lg border-solid border border-neutral-600 border-t-neutral-200
       drop-shadow-xl shadow-lg backdrop-blur-lg backdrop-brightness-75" 
-      :class="{'border-t-amber-200 border-amber-900 shadow-amber-200/40 backdrop-brightness-125 backdrop-sepia': selectedIdx === idx}">
+      :class="{'border-t-amber-200 border-amber-900 shadow-amber-200/40 backdrop-sepia backdrop-brightness-100': selectedIdx === idx}">
       <RouterLink :to="{ name: 'service', params: { id: service.name }}" class="service w-5/6 h-5/6 m-auto">
         <img class="h-full m-auto" :src="service.logo" :alt="service.name">
       </RouterLink>
